3ac6b976aa99e90377c30b986971c0b990a40e9e
codemaoz90 <codemaoz@gmail.com>
2021-04-08 22:18:57 +0200

    L

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Welcome from "./Welcome";
 import JokeApp from "./components/JokeApp";
 
