82ec5ac6f348e858d908d97ec4433b07ae1db617
codemaoz90 <codemaoz@gmail.com>
2021-04-08 22:17:12 +0200

    L

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from "react"
-import "./App.css"
-import { BrowserRouter as Router, Switch,Route} from "react-router-dom";
-import Welcome from "./Welcome"
-import JokeApp from "./components/JokeApp"
+import React from "react";
+import "./App.css";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import JokeApp from "./components/JokeApp";
 
-
-function App()  {
-
-    return (
-        <Router>
-            <Switch>
-                <Route path="/" exact>
-                    <Welcome/>
-                </Route>
-                <Route path="/jokes">                
-                    <JokeApp />
-                </Route>
-            </Switch>  
-        </Router>
-    )
+function App() {
+	return (
+		<Router basename={process.env.PUBLIC_URL}>
+			<Switch>
+				<Route path="/" exact>
+					<Welcome />
+				</Route>
+				<Route path="/jokes">
+					<JokeApp />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App
-
+export default App;
